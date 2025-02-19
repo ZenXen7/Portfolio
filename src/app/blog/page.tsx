5267e02d3ee9b44cb53e35ed2204653e2a1daee7
ZenXen7 <karlchristian.ajero@cit.edu>
2025-02-19 22:06:49 +0800

    Added Blogs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const metadata = {
   title: "Blog",
-  description: "My thoughts on software development, life, and more.",
+  description: "What happened recently.",
 };
 
 const BLUR_FADE_DELAY = 0.04;
@@ -15,7 +15,7 @@ export default async function BlogPage() {
   return (
     <section>
       <BlurFade delay={BLUR_FADE_DELAY}>
-        <h1 className="font-medium text-2xl mb-8 tracking-tighter">blog</h1>
+        <h1 className="font-medium text-2xl mb-8 tracking-tighter">KC Blogs</h1>
       </BlurFade>
       {posts
         .sort((a, b) => {
